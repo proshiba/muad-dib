7cd1d3989f11b86e849036d30be92b7ee77eadf4
DNSZLSK <155122610+DNSZLSK@users.noreply.github.com>
2026-01-01 20:49:29 +0100

    Ajout commande update IOCs

--- bin/muaddib.js
+++ bin/muaddib.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { run } = require('../src/index.js');
+const { updateIOCs } = require('../src/ioc/updater.js');
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -23,6 +24,7 @@ if (!command) {
   
   Usage:
     muaddib scan [path] [--json]    Analyse un projet
+    muaddib update                  Met a jour les IOCs
     muaddib help                    Affiche l'aide
   
   Options:
@@ -35,8 +37,16 @@ if (command === 'scan') {
   run(target, { json: jsonOutput }).then(exitCode => {
     process.exit(exitCode);
   });
+} else if (command === 'update') {
+  updateIOCs().then(() => {
+    process.exit(0);
+  }).catch(err => {
+    console.error('[ERREUR]', err.message);
+    process.exit(1);
+  });
 } else if (command === 'help') {
   console.log('muaddib scan [path] [--json] - Analyse un projet npm');
+  console.log('muaddib update - Met a jour les IOCs');
 } else {
   console.log(`Commande inconnue: ${command}`);
   process.exit(1);
