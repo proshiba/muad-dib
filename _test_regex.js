a8e83937583b339bfbd79a01c66b3e1516b5cd9c
DNSZLSK <155122610+DNSZLSK@users.noreply.github.com>
2026-03-06 14:24:22 +0100

    chore: remove temporary test scripts

--- _test_regex.js
+++
@@ -1,5 +0,0 @@
-const DIST_FILE_RE = /(?:^|[/\\])(?:dist|build)[/\\]|\.min\.js$|\.bundle\.js$/i;
-console.log('dist\\chunks\\file.js:', DIST_FILE_RE.test('dist\\chunks\\file.js'));
-console.log('dist/chunks/file.js:', DIST_FILE_RE.test('dist/chunks/file.js'));
-console.log('package\\dist\\file.js:', DIST_FILE_RE.test('package\\dist\\file.js'));
-console.log('src\\index.js:', DIST_FILE_RE.test('src\\index.js'));
